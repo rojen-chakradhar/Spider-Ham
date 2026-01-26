447bf93255b584d5248f488d579df69de693e0d2
Rojen Chakradhar <rojenchakradhar72@gmail.com>
2026-01-27 02:52:23 +0545

    bg in main menu added

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,12 +1,11 @@
 extends Control
 
-var button_type = null
-
 func _on_play_button_pressed() -> void:
 		get_tree().change_scene_to_file("res://scenes/Level_1.tscn")
 
+
 func _on_controls_button_pressed() -> void:
-		get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+		get_tree().change_scene_to_file("res://scenes/Controls.tscn")
 
 
 func _on_quit_button_pressed() -> void:
