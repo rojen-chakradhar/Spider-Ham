82ab64df61a468d0a36979a6ee54c6e0e4f5e1b4
Rojen Chakradhar <rojenchakradhar72@gmail.com>
2026-01-28 22:37:22 +0545

    boot splash and icon updated.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 260.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -250.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
